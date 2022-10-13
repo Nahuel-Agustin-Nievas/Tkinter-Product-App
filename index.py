d16468d0693cb10070e99e4c5dfbaa3974bd5f5b
Nahuel Nievas <nievasnahuel@gmail.com>
2022-10-14 00:47:11 +0200

    DB coded

--- index.py
+++ index.py
@@ -4,6 +4,9 @@ from tkinter import *
 import sqlite3
 
 class Product:
+     
+    db_name="database.db"
+
     def __init__(self, window):
         self.wind = window
         self.wind.title("Products Applications")
@@ -18,8 +21,45 @@ class Product:
         #name inputer
         Label(frame, text="Product Name:").grid(row = 1, column= 0)
         self.name = Entry(frame)
+        self.name.focus()   # focus on the labelFrame
         self.name.grid(row = 1, column = 1)
 
+        #price inputer
+        Label(frame, text="Product Price:").grid(row = 2, column = 0)
+        self.price = Entry(frame)
+        self.price.grid(row = 2, column = 1)
+
+
+        #button add product
+        ttk.Button(frame, text="Save Product").grid(row = 3, columnspan = 2, sticky= W + E)
+
+        #create table
+        self.tree = ttk.Treeview(height=10, column= 2)
+        self.tree.grid(row = 4, column = 0, columnspan = 2)
+        self.tree.heading("#0", text="Name", anchor=CENTER)
+        self.tree.heading("#1", text="Price", anchor=CENTER)
+
+
+        self.get_products()
+
+    def run_query(self, query, parameters =()):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            result= cursor.execute(query, parameters)
+            conn.commit()   
+            # data = cursor.fetchall()
+        return result
+
+    def get_products(self):
+        #cleaning table
+        records = self.tree.get_children()
+        for element in records:
+            self.tree.delete(element)
+        #quering database
+        query= "SELECT * FROM product ORDER BY name DESC"
+        db_rows = self.run_query(query)
+        for row in db_rows:
+            self.tree.insert("", 0, text=row[1], values= row[2])
 
 if __name__ == '__main__':
     window = Tk()
